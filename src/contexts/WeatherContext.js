465b98d65dc5a17e55bf9205793644b08a430924
Koray Guner <okorayguner@gmail.com>
2022-11-07 20:52:32 +0300

    edit | weatherContext

--- src/contexts/WeatherContext.js
+++ src/contexts/WeatherContext.js
@@ -12,25 +12,19 @@ export const WeatherProvider = ({children}) => {
     const apiKey = "4d496593dbb14471a8b831770f492325"
 
     useEffect( () => {
-            navigator.geolocation.getCurrentPosition(
-                async (position) => {
-                    await axios(
-                        `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=2eb5e66f28533bb0afdea44205243ec8&lang=tr`
-                    )
+            navigator.geolocation.getCurrentPosition( (position) => {
+                    axios(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=2eb5e66f28533bb0afdea44205243ec8&lang=tr`)
                     .then( (res) => res.data.name )
                     .then( (data) => setCity(data))
-                }
-            )
+                })
     },[])
 
     useEffect ( () => {
-        ( async () => {
-            await axios(`https://api.weatherbit.io/v2.0/forecast/daily?city=${city},TR&key=${apiKey}&lang=tr`)
+             axios(`https://api.weatherbit.io/v2.0/forecast/daily?city=${city},TR&key=${apiKey}&lang=tr`)
             .then( (res) => res.data.data)
             .then( (item) => setData(item.filter( (day, index) => index < 7)))
             .catch( (e) => console.log(e))
-        })()
-    }, [city])
+        }, [city])
 
 
     const values = {
